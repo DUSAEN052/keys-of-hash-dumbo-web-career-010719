23a9d55f14603610b0f5efba3efa5582c24a75b8
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-16 22:45:17 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,7 +5,7 @@ class Hash
     
     self.each do |key, val|
       puts "#{val}, #{arg}"
-      if val == arg.collect { |a| a}
+      if val == arg.each {|a| a}
         output << key
       end
     end
