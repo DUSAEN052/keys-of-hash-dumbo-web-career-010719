5bc3c61ca676c220f76a7b194c0bd7159b015a2c
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-16 20:07:42 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,8 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     output = []
-    
+    puts arguments
     arguments.each do |a|
-      puts "#{a}"
       output << hash.index(a)
     end
   output
